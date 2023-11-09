19241de6f07b9d3ec539d491491851a2047373d7
Michele Bucelli <michele.bucelli@polimi.it>
2023-11-09 20:38:17 +0000

    Add solution to lab-01

--- lab-01/src/lab-01.cpp
+++ lab-01/src/lab-01.cpp
@@ -5,4 +5,16 @@
 // Main function.
 int
 main(int /*argc*/, char * /*argv*/[])
-{}
+{
+  const unsigned int N = 39;
+  const unsigned int r = 1;
+
+  Poisson1D problem(N, r);
+
+  problem.setup();
+  problem.assemble();
+  problem.solve();
+  problem.output();
+
+  return 0;
+}
